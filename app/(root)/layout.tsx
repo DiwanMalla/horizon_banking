20eff9467a102e654a651af3e7c5613470558959
Diwan Malla <diwanmalla@192-168-1-3.tpgi.com.au>
2024-08-25 20:18:22 +1000

    Added and worked on navigation bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,30 @@
+import Sidebar from "@/components/ui/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main>SIDEBAR{children}</main>;
+  const loggedIn = {
+    $id: "unique-id",
+    email: "diwan@example.com",
+    userId: "user-id",
+    dwollaCustomerUrl: "https://api.dwolla.com/customers/unique-id",
+    dwollaCustomerId: "unique-dwolla-customer-id",
+    firstName: "Diwan",
+    lastName: "Malla",
+    name: "Diwan Malla",
+    address1: "123 Main St",
+    city: "New York",
+    state: "NY",
+    postalCode: "10001",
+    dateOfBirth: "1990-01-01",
+    ssn: "123-45-6789",
+  };
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      {children}
+    </main>
+  );
 }
