db6db5c0ef27b1bce04368eec36d8b4b59f7907c
Diwan Malla <diwanmalla@192-168-1-3.tpgi.com.au>
2024-08-25 19:28:18 +1000

    Added bank account detail cards with chartjs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,27 @@
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Diwan" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account transaction efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={2012.33}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
